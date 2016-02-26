c9a6b059f6f4a7dbf1917314b8a7455f1def8f4f
Thomas Read <thjread@gmail.com>
2016-02-26 20:13:08 +0000

    AsyncTask added

--- weathersync/src/main/java/thjread/dilithium/WeatherSync.java
+++ weathersync/src/main/java/thjread/dilithium/WeatherSync.java
@@ -1,6 +1,7 @@
 package thjread.dilithium;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -9,7 +10,22 @@ import com.google.android.gms.wearable.WearableListenerService;
 
 public class WeatherSync extends WearableListenerService {
     @Override
+    public void onCreate() {
+        Log.e("thjread.dilithium", "Weather Sync listener created");
+        WeatherSyncTask task = new WeatherSyncTask();
+        task.execute();
+    }
+
+    @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.e("thjread.dilithium", "Weather Sync message received");
+    }
+
+    private class WeatherSyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.e("thjread.dilithium", "Weather Sync task run");
+            return null;
+        }
     }
 }
