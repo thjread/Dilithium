34baa3be78f435b50d7ce75f228272ad3d853a85
Thomas Read <thjread@gmail.com>
2016-02-27 15:22:11 +0000

    Some communication working

---
+++ handheld/src/main/java/thjread/dilithium/WeatherSync.java
@@ -0,0 +1,16 @@
+package thjread.dilithium;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class WeatherSync extends Service {
+    public WeatherSync() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
